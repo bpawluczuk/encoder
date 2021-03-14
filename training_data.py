4d00bcbf9e23f2d40582cbc7b4410afb007b1069
bpawluczuk@itse.pl <haslobp1234>
2021-03-14 19:46:21 +0100

    change

--- training_data.py
+++ training_data.py
@@ -22,8 +22,8 @@ def get_training_data(images, batch_size):
         image = random_transform(image, **random_transform_args)
         warped_img, target_img = random_warp(image, size=128, scale=5, zoom=2)
 
-        cv2.imshow("warped_image", warped_img)
-        cv2.imshow("target_img", target_img)
+        # cv2.imshow("warped_image", warped_img)
+        # cv2.imshow("target_img", target_img)
 
         if i == 0:
             warped_images = numpy.empty((batch_size,) + warped_img.shape, warped_img.dtype)
@@ -32,5 +32,5 @@ def get_training_data(images, batch_size):
         warped_images[i] = warped_img
         target_images[i] = target_img
 
-    key = cv2.waitKey(0)
+    # key = cv2.waitKey(0)
     return warped_images, target_images
