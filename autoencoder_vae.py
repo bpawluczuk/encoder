1fb27799bd7e6e597d11d816d0fa034fdc0c1779
bpawluczuk <b.pawluczuk@companion.pl>
2021-03-20 19:41:34 +0100

    changes

--- autoencoder_vae.py
+++ autoencoder_vae.py
@@ -2,6 +2,7 @@ import cv2
 import numpy
 
 from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam
 
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
@@ -26,36 +27,43 @@ disable_eager_execution()
 IMAGE_SHAPE = (128, 128, 3)
 ENCODER_DIM = 1024
 
+optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
 
 class VAE:
     _image_shape = (128, 128, 3)
-    _latent_dim = 4
+    _latent_dim = 1024
+
+    def _sampling(self, args):
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], self._latent_dim), mean=0., stddev=1.)
+
+        return z_mean + K.exp(z_log_var) * epsilon
 
     def _get_z(self):
         input_img = Input(shape=self._image_shape)
+        print(K.int_shape(input_img))
 
-        x = Conv2D(32, 3, padding='same')(input_img)
+        x = Conv2D(128, 5, padding='same', strides=2)(input_img)
         x = LeakyReLU(0.1)(x)
-
         print(K.int_shape(x))
 
-        x = Conv2D(64, 3, padding='same', strides=2)(x)
+        x = Conv2D(256, 5, padding='same', strides=2)(x)
         x = LeakyReLU(0.1)(x)
+        print(K.int_shape(x))
 
-        x = Conv2D(64, 3, padding='same')(x)
+        x = Conv2D(512, 5, padding='same', strides=2)(x)
         x = LeakyReLU(0.1)(x)
+        print(K.int_shape(x))
 
-        x = Conv2D(64, 3, padding='same')(x)
+        x = Conv2D(1024, 5, padding='same', strides=2)(x)
         x = BatchNormalization()(x)
         x = LeakyReLU(0.1)(x)
         x = Dropout(0.4)(x)
 
         self._shape_before_flattening = K.int_shape(x)
-        print(K.int_shape(x))
 
-        x = Flatten()(x)
-        print(K.int_shape(x))
-        x = Dense(64, activation='relu')(x)
+        x = Dense(ENCODER_DIM, activation='relu')(Flatten()(x))
+        x = Dense(8 * 8 * 1024, activation='relu')(x)
 
         self.z_mean = Dense(self._latent_dim)(x)
         self.z_log_var = Dense(self._latent_dim)(x)
@@ -64,28 +72,32 @@ class VAE:
 
         return input_img, z
 
-    def _sampling(self, args):
-        z_mean, z_log_var = args
-        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], self._latent_dim), mean=0., stddev=1.)
-
-        return z_mean + K.exp(z_log_var) * epsilon
-
     def _get_decoded_z(self, z):
-        """ Decoder Network that decodes a latent space point z into an image """
-        decoder_input = Input(K.int_shape(z)[1:])
 
-        # Use a dense layer to psample to the correct number of units (exclude batch size with [1:])
+        decoder_input = Input(shape=K.int_shape(z)[1:])
+        x = decoder_input
+        print("decoder_input" + str(K.int_shape(x)))
+
         x = Dense(numpy.prod(self._shape_before_flattening[1:]), activation='relu')(decoder_input)
-
-        # Reshape into an image of the same shape as before the last Flatten layer in the encoder
         x = Reshape(self._shape_before_flattening[1:])(x)
 
-        # Apply reverse operation to the initial stack of Conv2D: a Conv2DTranspose
-        x = Conv2DTranspose(32, 3, padding='same', strides=2)(x)
+        x = Conv2DTranspose(512, 5, padding='same', strides=2)(x)
         x = LeakyReLU(0.1)(x)
+        print("decoder next" + str(K.int_shape(x)))
 
-        # We end up with a feature map of the same size as the original input
-        x = Conv2D(self._image_shape[-1], 3, padding='same', activation='sigmoid')(x)
+        x = Conv2DTranspose(256, 5, padding='same', strides=2)(x)
+        x = LeakyReLU(0.1)(x)
+        print("decoder next" + str(K.int_shape(x)))
+
+        x = Conv2DTranspose(128, 5, padding='same', strides=2)(x)
+        x = LeakyReLU(0.1)(x)
+        print("decoder next" + str(K.int_shape(x)))
+
+        x = Conv2DTranspose(128, 5, padding='same', strides=2)(x)
+        x = LeakyReLU(0.1)(x)
+        print("decoder next" + str(K.int_shape(x)))
+
+        x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
 
         decoder = Model(decoder_input, x)
         z_decoded = decoder(z)
@@ -102,20 +114,20 @@ class VAE:
     def _get_vae(self):
 
         input_img, z = self._get_z()
+        z_decoded = self._get_decoded_z(z)
 
-        z_decoded_A = self._get_decoded_z(z)
-        vae_A = Model(input_img, z_decoded_A)
-        loss = self._vae_loss(input_img, z_decoded_A)
-        vae_A.add_loss(loss)
-        vae_A.compile(optimizer='rmsprop', loss=self._vae_loss, experimental_run_tf_function=False)
+        autoencoder_A = Model(input_img, z_decoded)
+        loss = self._vae_loss(input_img, z_decoded)
+        autoencoder_A.add_loss(loss)
+        autoencoder_A.compile(optimizer=optimizer, loss=self._vae_loss, experimental_run_tf_function=False)
 
-        z_decoded_B = self._get_decoded_z(z)
-        vae_B = Model(input_img, z_decoded_B)
-        loss = self._vae_loss(input_img, z_decoded_B)
-        vae_B.add_loss(loss)
-        vae_B.compile(optimizer='rmsprop', loss=self._vae_loss, experimental_run_tf_function=False)
+        autoencoder_B = Model(input_img, z_decoded)
+        loss = self._vae_loss(input_img, z_decoded)
+        autoencoder_B.add_loss(loss)
+        autoencoder_B.compile(optimizer=optimizer, loss=self._vae_loss, experimental_run_tf_function=False)
 
-        return vae_A, vae_A
+        return autoencoder_A, autoencoder_B
+
 
 # ********************************************************************
 
@@ -129,7 +141,7 @@ autoencoder_A, autoencoder_B = VAE()._get_vae()
 # ********************************************************************
 
 # encoder.summary()
-# autoencoder_A.summary()
+autoencoder_A.summary()
 # autoencoder_B.summary()
 
 # ********************************************************************
