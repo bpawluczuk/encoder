af931c90fed496b0550dcb22ac71bf7c591c7892
bpawluczuk@itse.pl <haslobp1234>
2021-03-21 22:19:43 +0100

    change

--- autoencoder_vae_5.py
+++ autoencoder_vae_5.py
@@ -33,16 +33,16 @@ optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
 
 class VAE:
     _image_shape = (128, 128, 3)
-    _latent_dim = 1024
+    _latent_dim = 64
     _batch_size = 16
     _variational = 1
 
     def _conv(self, filters):
         def block(x):
             x = Conv2D(filters, kernel_size=5, strides=2, padding='same')(x)
-            x = BatchNormalization()(x)
+            # x = BatchNormalization()(x)
             x = LeakyReLU(0.1)(x)
-            x = Dropout(0.4)(x)
+            # x = Dropout(0.4)(x)
             return x
 
         return block
@@ -66,8 +66,8 @@ class VAE:
 
         x = self._conv(128)(_input)
         x = self._conv(256)(x)
-        x = self._conv(512)(x)
-        x = self._conv(1024)(x)
+        x = self._conv(256)(x)
+        x = self._conv(256)(x)
         x = Flatten()(x)
 
         if not self._variational:
@@ -78,58 +78,23 @@ class VAE:
 
             latent_space = Lambda(self._sampling)([self.z_mean, self.z_log_var])
 
-        x = Dense(8 * 8 * 1024, activation="relu")(latent_space)
-        x = Reshape((8, 8, 1024))(x)
-        encoder_output = self._upscale(512)(x)
+        x = Dense(8 * 8 * 256, activation="relu")(latent_space)
+        x = Reshape((8, 8, 256))(x)
+        encoder_output = self._upscale(256)(x)
         return Model(_input, encoder_output), self.z_mean, self.z_log_var
 
     def _decoder(self):
 
-        decoder_input = Input(shape=(16, 16, 512), name="decoder_input")
+        decoder_input = Input(shape=(16, 16, 256), name="decoder_input")
 
-        x = self._upscale(512)(decoder_input)
+        x = self._upscale(256)(decoder_input)
         x = self._upscale(256)(x)
         x = self._upscale(128)(x)
 
         decoder_output = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
         return Model(decoder_input, decoder_output)
 
-    def _vae_loss(self, input_img, z_decoded):
-        input_img = K.flatten(input_img)
-        z_decoded = K.flatten(z_decoded)
-        xent_loss = binary_crossentropy(input_img, z_decoded)
-        kl_loss = - 0.5 * K.mean(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
-        return K.mean(xent_loss + kl_loss)
-
-    def _get_vae(self):
-
-        encoder_input = Input(shape=self._image_shape, name="encoder_input")
-        encoder = self._encoder(encoder_input)
-        decoder_a = self._decoder()
-        decoder_b = self._decoder()
-
-        autoencoder_a = Model(encoder_input, decoder_a(encoder(encoder_input)))
-        if not self._variational:
-            autoencoder_a.compile(optimizer=optimizer, loss='mean_absolute_error')
-        else:
-            # loss = self._vae_loss(encoder_input, decoder_a(encoder(encoder_input)))
-            # autoencoder_a.add_loss(loss)
-            autoencoder_a.compile(optimizer=optimizer, loss=self._vae_loss)
-
-        autoencoder_b = Model(encoder_input, decoder_b(encoder(encoder_input)))
-        if not self._variational:
-            autoencoder_b.compile(optimizer=optimizer, loss='mean_absolute_error')
-        else:
-            # loss = self._vae_loss(encoder_input, decoder_b(encoder(encoder_input)))
-            # autoencoder_b.add_loss(loss)
-            autoencoder_b.compile(optimizer=optimizer, loss=self._vae_loss)
-
-        return encoder, autoencoder_a, autoencoder_a
-
-
 # ********************************************************************
-
-# encoder, autoencoder_A, autoencoder_B = VAE()._get_vae()
 
 encoder_input = Input(shape=IMAGE_SHAPE, name="encoder_input")
 encoder, z_mean, z_log_var = VAE()._encoder(encoder_input)
