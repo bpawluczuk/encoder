753409f658fe1f9f84bd8f54e3414ebb0e498fa0
bpawluczuk@itse.pl <haslobp1234>
2021-03-26 23:37:48 +0100

    change

--- autoencoder_vae.py
+++ autoencoder_vae.py
@@ -4,12 +4,13 @@ import numpy
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import Adam
 
+from tensorflow import keras
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
-from tensorflow.keras.metrics import binary_crossentropy
 
 from utils import get_image_paths, load_images, stack_images
 from training_data import get_training_data
+from pixel_shuffler import PixelShuffler
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -25,105 +26,143 @@ disable_eager_execution()
 # ********************************************************************
 
 IMAGE_SHAPE = (128, 128, 3)
-ENCODER_DIM = 512
+ENCODER_DIM = 1024
 
 optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
 
-class VAE:
-    _image_shape = (128, 128, 3)
-    _latent_dim = 512
+_image_shape = (128, 128, 3)
+_latent_dim = 256
+_batch_size = 16
+_variational = 1
+width = 128
+height = 128
 
-    def _sampling(self, args):
-        z_mean, z_log_var = args
-        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], self._latent_dim), mean=0., stddev=1.)
 
-        return z_mean + K.exp(z_log_var) * epsilon
-
-    def _get_z(self):
-        input_img = Input(shape=self._image_shape)
-        print(K.int_shape(input_img))
-
-        x = Conv2D(128, 5, padding='same', strides=2)(input_img)
+def conv(filters):
+    def block(x):
+        x = Conv2D(filters, kernel_size=5, strides=2, padding='same')(x)
         x = LeakyReLU(0.1)(x)
-        print(K.int_shape(x))
+        return x
 
-        x = Conv2D(256, 5, padding='same', strides=2)(x)
-        x = LeakyReLU(0.1)(x)
-        print(K.int_shape(x))
+    return block
 
-        x = Conv2D(512, 5, padding='same', strides=2)(x)
+
+def convDropout(filters):
+    def block(x):
+        x = Conv2D(filters, kernel_size=5, strides=2, padding='same')(x)
         x = BatchNormalization()(x)
         x = LeakyReLU(0.1)(x)
         x = Dropout(0.4)(x)
+        return x
 
-        self._shape_before_flattening = K.int_shape(x)
+    return block
 
-        x = Dense(ENCODER_DIM, activation='relu')(Flatten()(x))
-        x = Dense(8 * 8 * 512, activation='relu')(x)
 
-        self.z_mean = Dense(self._latent_dim)(x)
-        self.z_log_var = Dense(self._latent_dim)(x)
-
-        z = Lambda(self._sampling)([self.z_mean, self.z_log_var])
-
-        return input_img, z
-
-    def _get_decoded_z(self, z):
-
-        decoder_input = Input(shape=K.int_shape(z)[1:])
-        x = decoder_input
-        print("decoder_input" + str(K.int_shape(x)))
-
-        x = Dense(numpy.prod(self._shape_before_flattening[1:]), activation='relu')(decoder_input)
-        x = Reshape(self._shape_before_flattening[1:])(x)
-
-        x = Conv2DTranspose(512, 5, padding='same', strides=2)(x)
+def upscale(filters):
+    def block(x):
+        x = Conv2D(filters * 4, kernel_size=3, padding='same')(x)
         x = LeakyReLU(0.1)(x)
-        print("decoder next" + str(K.int_shape(x)))
+        x = PixelShuffler()(x)
+        return x
 
-        x = Conv2DTranspose(256, 5, padding='same', strides=2)(x)
-        x = LeakyReLU(0.1)(x)
-        print("decoder next" + str(K.int_shape(x)))
+    return block
 
-        x = Conv2DTranspose(128, 5, padding='same', strides=2)(x)
-        x = LeakyReLU(0.1)(x)
-        print("decoder next" + str(K.int_shape(x)))
 
-        x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], _latent_dim), mean=0., stddev=1.)
 
-        decoder = Model(decoder_input, x)
-        z_decoded = decoder(z)
+    return z_mean + K.exp(z_log_var) * epsilon
 
-        return z_decoded
 
-    def _vae_loss(self, input_img, z_decoded):
-        input_img = K.flatten(input_img)
-        z_decoded = K.flatten(z_decoded)
-        xent_loss = binary_crossentropy(input_img, z_decoded)
-        kl_loss = -5e-4 * K.mean(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
-        return K.mean(xent_loss + kl_loss)
+def vae_loss(input, x_decoded_mean):
+    mse_loss = K.mean(keras.losses.mse(input, x_decoded_mean), axis=(1, 2)) * height * width
+    kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
+    return mse_loss + kl_loss
 
-    def _get_vae(self):
 
-        input_img, z = self._get_z()
-        z_decoded = self._get_decoded_z(z)
+def Encoder(input_):
 
-        autoencoder_A = Model(input_img, z_decoded)
-        loss = self._vae_loss(input_img, z_decoded)
-        autoencoder_A.add_loss(loss)
-        autoencoder_A.compile(optimizer=optimizer, loss=self._vae_loss, experimental_run_tf_function=False)
+    x = conv(128)(input_)
+    x = conv(256)(x)
+    x = conv(512)(x)
+    x = convDropout(512)(x)
+    x = Flatten()(x)
 
-        autoencoder_B = Model(input_img, z_decoded)
-        loss = self._vae_loss(input_img, z_decoded)
-        autoencoder_B.add_loss(loss)
-        autoencoder_B.compile(optimizer=optimizer, loss=self._vae_loss, experimental_run_tf_function=False)
+    z_mean = Dense(_latent_dim)(x)
+    z_log_sigma = Dense(_latent_dim)(x)
 
-        return autoencoder_A, autoencoder_B
+    if not _variational:
+        latent_space = Dense(_latent_dim)(x)
+    else:
+        latent_space = Lambda(sampling)([z_mean, z_log_sigma])
+
+    x = Dense(8 * 8 * 512, activation="relu")(latent_space)
+    x = Reshape((8, 8, 512))(x)
+    x = upscale(512)(x)
+
+    return Model(input_, x), z_log_sigma, z_mean
+
+
+def Decoder():
+    input_ = Input(shape=(16, 16, 512))
+
+    x = upscale(512)(input_)
+    x = upscale(256)(x)
+    x = upscale(128)(x)
+
+    x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
+    return Model(input_, x)
 
 
 # ********************************************************************
 
-autoencoder_A, autoencoder_B = VAE()._get_vae()
+x = Input(shape=IMAGE_SHAPE)
+
+encoder, z_log_sigma, z_mean = Encoder(x)
+decoder_A = Decoder()
+decoder_B = Decoder()
+
+autoencoder_A = Model(x, decoder_A(encoder(x)))
+if not _variational:
+    autoencoder_A.compile(optimizer=optimizer, loss='mean_absolute_error')
+else:
+    autoencoder_A.compile(optimizer=optimizer, loss=vae_loss)
+
+autoencoder_B = Model(x, decoder_B(encoder(x)))
+if not _variational:
+    autoencoder_B.compile(optimizer=optimizer, loss='mean_absolute_error')
+else:
+    autoencoder_B.compile(optimizer=optimizer, loss=vae_loss)
+
+# ********************************************************************
+
+try:
+    if not _variational:
+        encoder.load_weights("models/AE/encoder.h5")
+        decoder_A.load_weights("models/AE/decoder_a.h5")
+        decoder_B.load_weights("models/AE/decoder_b.h5")
+    else:
+        encoder.load_weights("models/VAE/encoder.h5")
+        decoder_A.load_weights("models/VAE/decoder_a.h5")
+        decoder_B.load_weights("models/VAE/decoder_b.h5")
+    print("... load models")
+except:
+    print("models does not exist")
+
+
+def save_model_weights():
+    if not _variational:
+        encoder.save_weights("models/AE/encoder.h5")
+        decoder_A.save_weights("models/AE/decoder_a.h5")
+        decoder_B.save_weights("models/AE/decoder_b.h5")
+    else:
+        encoder.save_weights("models/VAE/encoder.h5")
+        decoder_A.save_weights("models/VAE/decoder_a.h5")
+        decoder_B.save_weights("models/VAE/decoder_b.h5")
+
+    print("save model weights")
+
 
 # ********************************************************************
 
@@ -151,7 +190,7 @@ for epoch in range(100000):
     print(epoch, loss_A, loss_B)
 
     if epoch % 100 == 0:
-        # save_model_weights()
+        save_model_weights()
         test_A = target_A[0:14]
         test_B = target_B[0:14]
 
