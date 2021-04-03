65cecb7271dd9c2d68a84e35ffb04fd2276b911f
bpawluczuk <b.pawluczuk@companion.pl>
2021-04-03 20:30:45 +0200

    changes

--- autoencoder_gan.py
+++ autoencoder_gan.py
@@ -30,100 +30,17 @@ ENCODER_DIM = 1024
 
 optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
 
-_image_shape = (128, 128, 3)
-_latent_dim = 256
-_batch_size = 16
-_variational = 1
-width = 128
-height = 128
-
-
-def conv(filters):
-    def block(x):
-        x = Conv2D(filters, kernel_size=5, strides=2, padding='same')(x)
-        x = LeakyReLU(0.1)(x)
-        return x
-
-    return block
-
-
-def convDropout(filters):
-    def block(x):
-        x = Conv2D(filters, kernel_size=5, strides=2, padding='same')(x)
-        x = BatchNormalization()(x)
-        x = LeakyReLU(0.1)(x)
-        x = Dropout(0.4)(x)
-        return x
-
-    return block
-
-
-def upscale(filters):
-    def block(x):
-        x = Conv2D(filters * 4, kernel_size=3, padding='same')(x)
-        x = LeakyReLU(0.1)(x)
-        x = PixelShuffler()(x)
-        return x
-
-    return block
-
-
-def sampling(args):
-    z_mean, z_log_var = args
-    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], _latent_dim), mean=0., stddev=1.)
-
-    return z_mean + K.exp(z_log_var) * epsilon
-
-
-def vae_loss(input, x_decoded_mean):
-    mse_loss = K.mean(keras.losses.mse(input, x_decoded_mean), axis=(1, 2)) * height * width
-    kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
-    return mse_loss + kl_loss
-
-
-def Encoder(input_):
-    x = conv(128)(input_)
-    x = conv(256)(x)
-    x = conv(512)(x)
-    x = convDropout(512)(x)
-    x = Flatten()(x)
-
-    z_mean = Dense(_latent_dim)(x)
-    z_log_sigma = Dense(_latent_dim)(x)
-
-    if not _variational:
-        latent_space = Dense(_latent_dim)(x)
-    else:
-        latent_space = Lambda(sampling)([z_mean, z_log_sigma])
-
-    x = Dense(8 * 8 * 512, activation="relu")(latent_space)
-    x = Reshape((8, 8, 512))(x)
-    x = upscale(512)(x)
-
-    return Model(input_, x), z_log_sigma, z_mean
-
-
-def Decoder():
-    input_ = Input(shape=(16, 16, 512))
-
-    x = upscale(512)(input_)
-    x = upscale(256)(x)
-    x = upscale(128)(x)
-
-    x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
-    return Model(input_, x)
-
 
 def Generator(input_):
     x = Dense(128 * 128 * 128)(input_)
     x = LeakyReLU(0.1)(x)
-    x = Reshape((128, 128, 2097152))(x)
+    x = Reshape((128, 128, 128))(x)
 
     x = Conv2D(256, 5, padding='same')(x)
     x = LeakyReLU(0.1)(x)
 
-    x = Conv2DTranspose(256, 4, strides=2, padding='same')(x)
-    x = LeakyReLU(0.1)(x)
+    # x = Conv2DTranspose(256, 4, strides=2, padding='same')(x)
+    # x = LeakyReLU(0.1)(x)
 
     x = Conv2D(256, 5, padding='same')(x)
     x = LeakyReLU(0.1)(x)
@@ -197,7 +114,6 @@ gan = keras.models.Model(gan_input, gan_output)
 gan_optimizer = keras.optimizers.RMSprop(lr=0.0004, clipvalue=1.0, decay=1e-8)
 gan.compile(optimizer=gan_optimizer, loss='binary_crossentropy')
 
-
 # gan_output = Discriminator(autoencoder_A(gan_input))
 # gan = Model(gan_input, gan_output)
 #
@@ -220,39 +136,49 @@ images_B = load_images(images_B) / 255.0
 
 images_A += images_B.mean(axis=(0, 1, 2)) - images_A.mean(axis=(0, 1, 2))
 
-# for epoch in range(10):
-#     batch_size = 16
-#     warped_A, target_A = get_training_data(images_A, batch_size)
-#     warped_B, target_B = get_training_data(images_B, batch_size)
-#
-#     loss_A = autoencoder_A.train_on_batch(warped_A, target_A)
-#     loss_B = autoencoder_B.train_on_batch(warped_B, target_B)
-#     print(epoch, loss_A, loss_B)
-#
-#     if epoch % 100 == 0:
-#         save_model_weights()
-#         test_A = target_A[0:14]
-#         test_B = target_B[0:14]
-#
-#     figure_A = numpy.stack([
-#         test_A,
-#         autoencoder_A.predict(test_A),
-#         autoencoder_B.predict(test_A),
-#     ], axis=1)
-#
-#     figure_B = numpy.stack([
-#         test_B,
-#         autoencoder_B.predict(test_B),
-#         autoencoder_A.predict(test_B),
-#     ], axis=1)
-#
-#     figure = numpy.concatenate([figure_A, figure_B], axis=0)
-#     figure = figure.reshape((4, 7) + figure.shape[1:])
-#     figure = stack_images(figure)
-#
-#     figure = numpy.clip(figure * 255, 0, 255).astype('uint8')
-#
-#     cv2.imshow("", figure)
-#     key = cv2.waitKey(1)
+start = 0
+for epoch in range(10):
+    warped_A, target_A = get_training_data(images_A, batch_size)
+
+    random_latent_vectors = numpy.random.normal(size=(batch_size, latent_dim))
+    generated_images = generator.predict(random_latent_vectors)
+
+    combined_images = numpy.concatenate([generated_images, target_A])
+
+    labels = numpy.concatenate([valid, fake])
+    labels += 0.05 * numpy.random.random(labels.shape)
+
+    d_loss = discriminator.train_on_batch(combined_images, labels)
+
+    random_latent_vectors = numpy.random.normal(size=(batch_size, latent_dim))
+    misleading_targets = numpy.zeros((batch_size, 1))
+
+    a_loss = gan.train_on_batch(random_latent_vectors, misleading_targets)
+
+    print('strata dyskryminatora w kroku %s: %s' % (epoch, d_loss))
+    print('strata przeciwna: %s: %s' % (epoch, a_loss))
+
+    # img = image.array_to_img(generated_images[0] * 255., scale=False)
+    # img = image.array_to_img(target_A[0] * 255., scale=False)
+
+    # print(noise.shape)
+
+    # d_loss_real = discriminator.train_on_batch(target_A, valid)
+    # d_loss_fake = discriminator.train_on_batch(gen_imgs, fake)
+    # d_loss = 0.5 * numpy.add(d_loss_real, d_loss_fake)
+
+    figure_A = numpy.stack([
+        generated_images,
+        target_A
+    ], axis=1)
+
+    figure = numpy.concatenate([figure_A], axis=0)
+    # figure = figure.reshape((4, 7) + figure.shape[1:])
+    figure = stack_images(figure)
+
+    figure = numpy.clip(figure * 255, 0, 255).astype('uint8')
+
+    cv2.imshow("", figure)
+    key = cv2.waitKey(1)
 
 # ********************************************************************
