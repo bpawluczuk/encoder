45f073ee12a0709678c4cd8a9209f32d29824a00
bpawluczuk@itse.pl <haslobp1234>
2021-10-13 20:16:51 +0200

    change

--- autoencoder_ae_vae_test.py
+++ autoencoder_ae_vae_test.py
@@ -30,15 +30,15 @@ size = 256
 zoom = 4  # 64*zoom
 width = 256
 height = 256
-_latent_dim = 256
+_latent_dim = 256 #128
 _variational = 0
 chanels = 3
-batch_size = 2
+batch_size = 1
 
 IMAGE_SHAPE = (size, size, chanels)
 ENCODER_DIM = 1024
 
-optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.9)
+optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.95)
 
 kernel_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
@@ -73,7 +73,7 @@ def convDropout(filters, kernel_size=4, strides=2):
         )(x)
         x = tfa.layers.InstanceNormalization(gamma_initializer=gamma_init)(x)
         x = LeakyReLU(0.1)(x)
-        x = Dropout(0.4)(x)
+        # x = Dropout(0.4)(x)
         return x
 
     return block
@@ -122,19 +122,18 @@ def Encoder(input_):
     else:
         latent_space = Lambda(sampling)([z_mean, z_log_sigma])
 
-    x = Dense(8 * 8 * 64, activation="relu")(latent_space)
-    x = Reshape((8, 8, 64))(x)
-    x = upscale(64)(x)
+    x = Dense(16 * 16 * 128, activation="relu")(latent_space)
+    x = Reshape((16, 16, 128))(x)
+    x = upscale(512)(x)
 
     return Model(input_, x), z_log_sigma, z_mean
 
 
 def Decoder():
-    input_ = Input(shape=(16, 16, 64))
-    x = upscale(64)(input_)
-    x = upscale(128)(x)
+    input_ = Input(shape=(32, 32, 512))
+    x = upscale(512)(input_)
     x = upscale(256)(x)
-    x = upscale(512)(x)
+    x = upscale(128)(x)
 
     x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(x)
     return Model(input_, x)
@@ -191,14 +190,14 @@ def save_model_weights():
 
 # ********************************************************************
 
-# encoder.summary()
+encoder.summary()
 # autoencoder_A.summary()
 # autoencoder_B.summary()
 
 # ********************************************************************
 
-images_A = get_image_paths("data_test/OL/trainOL")
-images_B = get_image_paths("data_test/LU/trainLU")
+images_A = get_image_paths("data/OL/trainOL")
+images_B = get_image_paths("data/LU/trainLU")
 images_A = load_images(images_A) / 255.0
 images_B = load_images(images_B) / 255.0
 
