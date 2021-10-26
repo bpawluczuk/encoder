8883271c45e749c14e222636c40884892e00bcc0
bpawluczuk <bpawluczuk@itse.pl>
2021-10-26 19:42:19 +0200

    changes

--- ae.py
+++ ae.py
@@ -1,25 +1,24 @@
+import datetime
+
 import cv2
 import numpy
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from PIL import Image
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import Adam
 
 from tensorflow import keras
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
-import tensorflow_addons as tfa
 from tensorflow.python.framework.ops import disable_eager_execution
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+import tensorflow_addons as tfa
+from tensorflow.keras import layers
 
 from lib.utils import get_image_paths, load_images, stack_images
 from lib.training_data import get_training_data
 from lib.pixel_shuffler import PixelShuffler
-
-import tensorflow_addons as tfa
-import tensorflow_datasets as tfds
 
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -35,353 +34,190 @@ try:
 except:
     pass
 
-# disable_eager_execution()
-
 # ********************************************************************
 
 size = 256
-zoom = 4  # 64*zoom
 width = 256
 height = 256
-_latent_dim = 256  # 128
-_variational = 0
 chanels = 3
-buffer_size = 4
-batch_size = 1
 
 IMAGE_SHAPE = (size, size, chanels)
-image_size = (256, 256)
 ENCODER_DIM = 1024
 
-adam_optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.99)
+_latent_dim = 512  # 128
+_variational = 0
+
+zoom = 4  # 64*zoom
+
+optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.95)
 
 kernel_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 
-# *********************************************************************
-
-tfds.disable_progress_bar()
-autotune = tf.data.experimental.AUTOTUNE
+disable_eager_execution()
 
 
-# *********************************************************************
+# ********************************************************************
 
-def normalize_img(img):
-    img = tf.cast(img, dtype=tf.float32)
-    return img / 255.0
-
-
-# *********************************************************************
-
-train_ol = tf.keras.preprocessing.image_dataset_from_directory(
-    "data_train/OL_NEW",
-    seed=1,
-    label_mode=None,
-    shuffle=buffer_size,
-    image_size=image_size,
-    batch_size=batch_size,
-)
-
-train_ol = (
-    train_ol.map(normalize_img, num_parallel_calls=autotune).cache().shuffle(buffer_size)
-)
-
-plt.figure(figsize=(10, 10))
-for i, img in enumerate(train_ol.take(9)):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(img[0])
-    plt.axis("off")
-
-plt.show()
-plt.close()
-
-# *********************************************************************
-
-validation_data_ol = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
-validation_ol = validation_data_ol.flow_from_directory(
-    'data_train/OL_NEW',
-    target_size=(255, 255),
-    batch_size=32,
-    class_mode='binary')
-
-images, labels = next(validation_ol)
-
-plt.figure(figsize=(10, 10))
-for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i])
-    plt.axis("off")
-
-plt.show()
-plt.close()
-
-# *********************************************************************
-
-train_lu = tf.keras.preprocessing.image_dataset_from_directory(
-    "data_train/LU_NEW",
-    seed=1,
-    label_mode=None,
-    shuffle=buffer_size,
-    image_size=image_size,
-    batch_size=batch_size,
-)
-
-train_lu = (
-    train_lu.map(normalize_img, num_parallel_calls=autotune).shuffle(buffer_size)
-)
-
-plt.figure(figsize=(10, 10))
-for i, img in enumerate(train_lu.take(9)):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(img[0])
-    plt.axis("off")
-
-plt.show()
-plt.close()
-
-# *********************************************************************
-
-validation_data_lu = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
-validation_lu = validation_data_lu.flow_from_directory(
-    'data_train/LU_NEW',
-    target_size=(255, 255),
-    batch_size=32,
-    class_mode='binary')
-
-images, labels = next(validation_lu)
-
-plt.figure(figsize=(10, 10))
-for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i])
-    plt.axis("off")
-
-plt.show()
-plt.close()
-
-
-# *********************************************************************
-
-def conv(filters, kernel_size=3, strides=2):
+def conv(filters, kernel_size=4, strides=2, padding='same', activation=True, dropout_rate=0):
     def block(x):
+
         x = Conv2D(
             filters,
             kernel_size=kernel_size,
             strides=strides,
-            padding='same',
+            padding=padding,
             kernel_initializer=kernel_init
         )(x)
+
         x = tfa.layers.InstanceNormalization(gamma_initializer=gamma_init)(x)
-        x = LeakyReLU(0.1)(x)
+
+        if activation:
+            x = LeakyReLU(0.1)(x)
+
+        if dropout_rate:
+            x = Dropout(dropout_rate)(x)
+
         return x
 
     return block
 
 
-def convDropout(filters, kernel_size=3, strides=2):
-    def block(x):
-        x = Conv2D(
-            filters,
-            kernel_size=kernel_size,
-            strides=strides,
-            padding='same',
-            kernel_initializer=kernel_init
-        )(x)
-        x = tfa.layers.InstanceNormalization(gamma_initializer=gamma_init)(x)
-        x = LeakyReLU(0.1)(x)
-        # x = Dropout(0.2)(x)
-        return x
-
-    return block
+def convInOut(filters, kernel_size=4, padding='same', activation=True, dropout_rate=0):
+    return conv(
+        filters=filters,
+        kernel_size=kernel_size,
+        strides=1,
+        padding=padding,
+        activation=activation,
+        dropout_rate=dropout_rate
+    )
 
 
-def upscale(filters, filter_times=4, kernel_size=3, name=""):
+def downscale(filters, kernel_size=4, strides=2, padding='same', activation=True, dropout_rate=0):
+    return conv(
+        filters=filters // 2,
+        kernel_size=kernel_size,
+        strides=strides,
+        padding=padding,
+        activation=activation,
+        dropout_rate=dropout_rate
+    )
+
+
+def upscale(filters, kernel_size=4, filter_times=2, padding='same', activation=True):
     def block(x):
         x = Conv2D(
             filters * filter_times,
             kernel_size=kernel_size,
-            padding='same',
-            kernel_initializer=kernel_init,
-            name=name
+            padding=padding,
+            kernel_initializer=kernel_init
         )(x)
-        x = LeakyReLU(0.1)(x)
+
+        if activation:
+            x = LeakyReLU(0.1)(x)
+
         x = PixelShuffler()(x)
+
         return x
 
     return block
 
 
 def Encoder(input_, name="Encoder"):
-    x = conv(64, strides=2, kernel_size=7)(input_)
-    x = conv(128, strides=2)(x)
-    x = conv(128, strides=1)(x)
-    x = conv(256, strides=2)(x)
-    x = conv(256, strides=1)(x)
-    x = convDropout(512, strides=2)(x)
+    x = convInOut(256, kernel_size=8)(input_)
+    x = conv(512)(x)
+    x = conv(512, strides=1)(x)
+    x = conv(1024)(x)
+    x = conv(1024, strides=1)(x)
     x = Flatten()(x)
 
     latent_space = Dense(_latent_dim)(x)
 
-    x = Dense(16 * 16 * 128, activation="relu")(latent_space)
-    x = Reshape((16, 16, 128))(x)
-    x = upscale(512, filter_times=4)(x)
+    x = Dense(32 * 32 * 1024, activation="relu")(latent_space)
+    x = Reshape((32, 32, 1024))(x)
+    x = upscale(1024, filter_times=4)(x)
 
-    encoder = Model(input_, x, name=name)
-    encoder.summary()
-
-    return encoder
+    return Model(input_, x, name=name)
 
 
 def Decoder(name="Decoder"):
-    input_ = Input(shape=(32, 32, 512))
+    input_ = Input(shape=(64, 64, 1024))
 
-    x = upscale(256, filter_times=2, name="decoder_up_1")(input_)
-    x = upscale(128, filter_times=2, name="decoder_up_2")(x)
-    x = upscale(64, filter_times=4, name="decoder_up_3")(x)
+    x = upscale(1024)(input_)
+    x = conv(1024, strides=1)(x)
+    x = upscale(512)(x)
+    x = conv(512, strides=1)(x)
 
-    x = Conv2D(3, kernel_size=7, padding='same', activation='sigmoid')(x)
+    x = convInOut(chanels, kernel_size=8)(x)
+    x = layers.Activation("sigmoid")(x)
 
-    decoder = Model(input_, x, name=name)
-    decoder.summary()
-
-    return decoder
-
-
-# *********************************************************************
-
-loss_tracker = keras.metrics.Mean(name="loss")
-mae_metric = keras.metrics.MeanAbsoluteError(name="mae")
+    return Model(input_, x, name=name)
 
 
-class AutoEncoder(keras.Model):
+# ********************************************************************
 
-    def __init__(self, auto_encoder_A, auto_encoder_B, optimizer_A, optimizer_B):
-        super(AutoEncoder, self).__init__()
-        self.auto_encoder_A = auto_encoder_A
-        self.auto_encoder_B = auto_encoder_B
-        self.optimizer_A = optimizer_A
-        self.optimizer_B = optimizer_B
-        self.total_loss_tracker_A = keras.metrics.Mean(name="total_loss")
-        self.total_loss_tracker_B = keras.metrics.Mean(name="total_loss")
+x = Input(shape=IMAGE_SHAPE)
 
-    def train_step(self, data):
-        x, y = data
+encoder = Encoder(x)
+decoder_A = Decoder()
+decoder_B = Decoder()
 
-        with tf.GradientTape(persistent=True) as tape:
-            reconstruction_A = self.auto_encoder_A(x, training=True)
-            total_loss_A = keras.losses.mean_squared_error(x, reconstruction_A)
+# ********************************************************************
 
-            reconstruction_B = self.auto_encoder_B(y, training=True)
-            total_loss_B = keras.losses.mean_squared_error(y, reconstruction_B)
+autoencoder_A = Model(x, decoder_A(encoder(x)))
+autoencoder_A.compile(optimizer=optimizer, loss='mae', metrics=['accuracy'])
 
-        gradients_A = tape.gradient(total_loss_A, self.auto_encoder_A.trainable_weights)
-        self.optimizer_A.apply_gradients(zip(gradients_A, self.auto_encoder_A.trainable_weights))
-        self.total_loss_tracker_A.update_state(total_loss_A)
+autoencoder_B = Model(x, decoder_B(encoder(x)))
+autoencoder_B.compile(optimizer=optimizer, loss='mae', metrics=['accuracy'])
 
-        gradients_B = tape.gradient(total_loss_B, self.auto_encoder_B.trainable_weights)
-        self.optimizer_B.apply_gradients(zip(gradients_B, self.auto_encoder_B.trainable_weights))
-        self.total_loss_tracker_B.update_state(total_loss_B)
+# encoder.summary()
+# autoencoder_A.summary()
+# autoencoder_B.summary()
 
-        return {
-            "loss_A": self.total_loss_tracker_A.result(),
-            "loss_B": self.total_loss_tracker_B.result()
-        }
+# ********************************************************************
+
+try:
+    encoder.load_weights("models/AE/encoder.h5")
+    decoder_A.load_weights("models/AE/decoder_a.h5")
+    decoder_B.load_weights("models/AE/decoder_b.h5")
+    print("... load model")
+except:
+    print("model does not exist")
 
 
-class Monitor(keras.callbacks.Callback):
+def save_model_weights():
+    encoder.save_weights("models/AE/encoder.h5")
+    decoder_A.save_weights("models/AE/decoder_a.h5")
+    decoder_B.save_weights("models/AE/decoder_b.h5")
+    print("save model weights")
 
-    def __init__(self, num_img=4):
-        self.num_img = num_img
+# ********************************************************************************
 
-    def on_batch_begin(self, batch, logs=None):
+images_A = get_image_paths("data_test/OL/trainOL")
+images_B = get_image_paths("data_test/LU/trainLU")
+images_A = load_images(images_A) / 255.0
+images_B = load_images(images_B) / 255.0
 
-        for i, img in enumerate(train_ol.take(self.num_img)):
-            prediction = self.model.auto_encoder_A(img, training=False)[0].numpy()
+images_A += images_B.mean(axis=(0, 1, 2)) - images_A.mean(axis=(0, 1, 2))
 
-            prediction = (prediction * 255).astype(numpy.uint8)
-            img = (img[0] * 255).numpy().astype(numpy.uint8)
+batch_size = 2
+epochs = 2
+dataset_size = len(images_A)
+batches = round(dataset_size / batch_size)
+sample_interval = 100
 
-            figure = numpy.stack((img, prediction))
-            figure = numpy.concatenate(figure, axis=1)
+# ********************************************************************************
 
-            cv2.imshow("olTolu", figure)
+start_time = datetime.datetime.now()
 
-        for i, img in enumerate(train_lu.take(self.num_img)):
-            prediction = self.model.auto_encoder_B(img, training=False)[0].numpy()
+valid = numpy.ones((batch_size,) + (32, 32, 1))
+fake = numpy.zeros((batch_size,) + (32, 32, 1))
 
-            prediction = (prediction * 255).astype(numpy.uint8)
-            img = (img[0] * 255).numpy().astype(numpy.uint8)
+for epoch in range(epochs):
+    epoch += 1
+    for batch in range(batches):
+        batch += 1
 
-            figure = numpy.stack((img, prediction))
-            figure = numpy.concatenate(figure, axis=1)
-
-            cv2.imshow("luTool", figure)
-
-        cv2.waitKey(1)
-
-    def on_epoch_end(self, epoch, logs=None):
-        _, ax = plt.subplots(4, 2, figsize=(12, 12))
-        for i, img in enumerate(train_ol.take(self.num_img)):
-            prediction = self.model.auto_encoder_B(img, training=False)[0].numpy()
-            prediction = (prediction * 255).astype(numpy.uint8)
-            img = (img[0] * 255).numpy().astype(numpy.uint8)
-
-            ax[i, 0].imshow(img)
-            ax[i, 1].imshow(prediction)
-            ax[i, 0].set_title("Input image")
-            ax[i, 1].set_title("Translated image")
-            ax[i, 0].axis("off")
-            ax[i, 1].axis("off")
-
-        plt.show()
-        plt.close()
-
-
-plotter = Monitor()
-
-
-# *********************************************************************
-
-def get_model():
-    x = Input(shape=IMAGE_SHAPE)
-
-    encoder = Encoder(x)
-
-    decoder_A = Decoder()
-    auto_encoder_A = Model(x, decoder_A(encoder(x)))
-
-    decoder_B = Decoder()
-    auto_encoder_B = Model(x, decoder_B(encoder(x)))
-
-    model = AutoEncoder(
-        auto_encoder_A=auto_encoder_A,
-        auto_encoder_B=auto_encoder_B,
-        optimizer_A=Adam(lr=5e-5, beta_1=0.5, beta_2=0.99),
-        optimizer_B=Adam(lr=5e-5, beta_1=0.5, beta_2=0.99)
-    )
-
-    model.compile()
-
-    return model
-
-
-# *********************************************************************
-
-auto_encoder_A = get_model()
-
-auto_encoder_A.fit(
-    tf.data.Dataset.zip((train_ol, train_lu)),
-    validation_data=(validation_ol, validation_lu),
-    epochs=2,
-    callbacks=[plotter]
-)
+        warped_A, target_A = get_training_data(images_A, batch_size, size, zoom)
+        warped_B, target_B = get_training_data(images_B, batch_size, size, zoom)
